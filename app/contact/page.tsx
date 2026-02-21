362b700134478ced9642661733b4940c1840fba4
Jaya Prakash <jayaprakashmajhi26@gmail.com>
2026-02-21 23:31:54 +0530

    Updated schedule page with event dates and registration deadline

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -11,25 +11,25 @@ const eventCoordinators = [
     {
         event: "Art & Craft",
         faculty: "Prof. Namrata Mishra",
-        facultyPhone: "9282738696",
-        studentCoordinators: ["Pratiyush Dash", "Ganesh Pradhan", "Bhrayal Sahoo"],
-        contacts: ["7202645050", "7326814760", "8018184949"],
+        facultyPhone: "7978528696",
+        studentCoordinators: ["Subhrajyoti Hota", "Pratyush Dash", "Ganesh Pradhan", "Binayak Sahoo"],
+        contacts: ["9692240722", "7205464504", "7326814760", "8018184949"],
         year: "2nd"
     },
     {
-        event: "Theme Art",
-        faculty: "Prof Irani Majundar",
+        event: "Theme Act - Unity in Diversity",
+        faculty: "Prof.Irani Majundar",
         facultyPhone: "7008848775",
-        studentCoordinators: ["Sayan Sourav Sahoo", "Abhilash Kumari", "Abhinandan Mahanta", "Subham Anubhav Dhal"],
+        studentCoordinators: ["Sayan Sourav Sahoo", "Abhilasa Kumari", "Abhinandan Mahanta", "Subham Anubhav Dhal"],
         contacts: ["9040040724", "9135503321", "9040498327", "7853839858"],
         year: "2nd"
     },
     {
         event: "Frame the Fame",
         faculty: "Prof Chakardhar Karan",
-        facultyPhone: "9935662133",
-        studentCoordinators: ["Prem kumar Beura", "Ritesh Das", "Aish Chakra", "Chandrashekhar Sarangi", "Subhankar Panigrahi"],
-        contacts: ["9348958998", "7681869791", "6370062911", "8926974373", "7853652678"],
+        facultyPhone: "9938662133",
+        studentCoordinators: ["Prem kumar Beura", "Ritesh Das", "Asish Chakra", "Chandrashekhar Sarangi", "Subhankar Panigrahi"],
+        contacts: ["9348958998", "7681867791", "6370657611", "8926374373", "7853054976"],
         year: "2nd"
     },
     {
@@ -37,71 +37,71 @@ const eventCoordinators = [
         faculty: "Prof Binayak Mishra",
         facultyPhone: "8093539537",
         studentCoordinators: ["Satya sundar Giri", "Jayaprakash Majhi", "Mrutyunjay Swain", "Manas Ranjan Rana", "Satyajit Bal", "Santosh Kumar Rout"],
-        contacts: ["8456033655", "7205089574", "7609910671", "9938070761", "7978252667", "7838858095"],
+        contacts: ["8456033655", "7205089574", "7609910671", "9938070761", "7978253647", "7838858095"],
         year: "3rd"
     },
     {
         event: "Face Fantasy",
-        faculty: "prof.Anjali Sahu",
+        faculty: "Prof.Anjali Sahu",
         facultyPhone: "9776294542",
-        studentCoordinators: ["Pratikshya Das", "Pritam Priyadarshi", "Roshan Ayush", "Waris Raj"],
-        contacts: ["7978466012", "9672225267", "7858288329", "9835400188"],
+        studentCoordinators: ["Pratikshya Das", "Pritam Priyadarshi", "Shakti Ayach", "Waris Raj"],
+        contacts: ["7978466012", "9692225267", "7855877814", "9835400188"],
         year: "2nd"
     },
     {
         event: "Rangstav - The Art of Colors",
         faculty: "Prof.Anjali Sahu",
         facultyPhone: "9776294542",
-        studentCoordinators: ["Rishi kumar Sahoo", "Sania Mishra", "Ankita Jena", "Kanishka kumari"],
-        contacts: ["8984801480", "7698529919", "6371096234", "8107220500"],
+        studentCoordinators: ["Rishi Kumar Sahoo", "Sania Mishra", "Ankita Jena", "Kanishka Kumari"],
+        contacts: ["8984803480", "7894529919", "6371096234", "8102720500"],
         year: "2nd"
     },
     {
         event: "Esports",
         faculty: "Prof Himanshu Bhusan Mohapatra",
         facultyPhone: "7978552954",
-        studentCoordinators: ["Sushil Parida", "Paresh kumar Jena", "Matru prasad Moharty", "Rajnalan Samal", "Abhisek Dash", "Bimit Ranjan Nayak"],
-        contacts: ["7894547073", "9302391275", "9337734500", "8760076329", "8248890306", "7250943171"],
+        studentCoordinators: ["Sushil Parida", "Paresh Kumar Jena", "Matru Prasad Mohanty", "Rajmilan Samal", "Abhisek Dash", "Binit Ranjan Nayak", "Rishi kumar Bagria", "Rishabh Kumar"],
+        contacts: ["7894547073", "9302391275", "9337734580", "8260676329", "8249890306", "7750943171", "8349798746", "7250056887"],
         year: "3rd"
     },
     {
         event: "Flavour Fiesta",
-        faculty: "prof Dibyalisha Rath",
+        faculty: "Prof Dibyalisha Rath",
         facultyPhone: "7894196460",
-        studentCoordinators: ["Niharika Sahu", "Aanksha Panda", "Sunil Pradhan", "Swetapadma Sahoo"],
-        contacts: ["9500938704", "7605985406", "7602986014", "8148025315"],
+        studentCoordinators: ["Niharika Sahu", "Aanksha Panda", "Sunil Pradhan", "Sweatapadma Sahoo"],
+        contacts: ["9508938704", "7605985406", "7609906034", "8144955215"],
         year: "2nd"
     },
     {
         event: "Treasure Hunt",
         faculty: "Prof. Ambika Prasad Hota",
         facultyPhone: "9439687716",
-        studentCoordinators: ["Biswaprakash Rout", "Suvam Khatoi", "krishna Prasad Dhal", "Ashutosh Jena", "Amit Karan", "Swayamprabha Padhi"],
-        contacts: ["8984676600", "9346461420", "8260736828", "7847028928", "6207086905", "8144023632"],
+        studentCoordinators: ["Biswaprakash Rout", "Suvam Khatoi", "Krishna Prasad Dhal", "Ashutosh Jena", "Amit Karan", "Swayanprabha Padhi"],
+        contacts: ["8984676600", "9346461420", "8260736828", "7847028928", "6207080025", "8144023632"],
         year: "2nd"
     },
     {
         event: "Pot Design",
         faculty: "Prof.Smruti Das",
         facultyPhone: "8800853174",
-        studentCoordinators: ["Swatisudia Routray", "Juel Jaslan Sahoo", "Aparajita Nanda", "Situnarayan Dash"],
-        contacts: ["8260116073", "8260818040", "9693251228", "7894894171"],
+        studentCoordinators: ["Swatisudha Routray", "Juel Jaslin Sahoo", "Aparajita Nanda", "Sibunarayan Dash"],
+        contacts: ["8260116073", "8260818040", "9692951228", "7894894171"],
         year: "2nd"
     },
     {
         event: "Laugh Riot",
-        faculty: "Prof. Debarand Sahu",
+        faculty: "Prof. Debanand Sahu",
         facultyPhone: "7978773214",
-        studentCoordinators: ["Rishi kumar Bagria", "Soumyajit Biswal", "Sania Mishra", "Soepak Roy"],
-        contacts: ["8349598746", "7751990539", "7894529919", "9832762097"],
+        studentCoordinators: ["Rishi kumar Bagria", "Soumyajit Biswal", "Sania Mishra", "Sagnik Roy"],
+        contacts: ["8349798746", "7751990459", "7894529919", "9832762097"],
         year: "1st"
     },
     {
         event: "Talent Spotlight",
-        faculty: "Prof. Bishupada Roul",
+        faculty: "Prof. Bishnupada Roul",
         facultyPhone: "9776535516",
-        studentCoordinators: ["T Priyanka Patra", "Tejawsi Raj", "Archita Sahoo", "Priti Pragyan Biswal"],
-        contacts: ["9872421482", "9263378668", "7335093978", "9771124371"],
+        studentCoordinators: ["T.Priyanka Patra", "Tejaswi Raj", "Archita Sahoo", "Priti Pragyan Biswal"],
+        contacts: ["9827421482", "9263378468", "7735093979", "9777124371"],
         year: "2nd"
     },
     {
@@ -109,23 +109,23 @@ const eventCoordinators = [
         faculty: "Prof. Satyapirya Swain",
         facultyPhone: "7684850953",
         studentCoordinators: ["Om Prakash Parida", "Sujan Das", "Amar Aryan", "Sambit Nayak"],
-        contacts: ["7008997487", "8926097825", "9777136478", "9861250898"],
+        contacts: ["7008997487", "8926097825", "9777336479", "9861250898"],
         year: "2nd"
     },
     {
         event: "Mini Movie Fest",
-        faculty: "prof. Jyoti Panda",
+        faculty: "Prof. Jyoti Panda",
         facultyPhone: "7735025428",
-        studentCoordinators: ["Smruti Ranjan Pradhan", "Snikey Dutta", "Subham Pradhan", "Smitanajali Nayak", "Krish Nayak"],
-        contacts: ["9917947198", "9279652490", "9137963411", "8658053113", "9124030027"],
+        studentCoordinators: ["Smruti Ranjan Pradhan", "Rinkey Dutta", "Subham Pradhan", "Smitanjali Nayak", "Arpit Nayak"],
+        contacts: ["9937947198", "9279652403", "9337963411", "8658003113", "9124003027"],
         year: "2nd"
     },
     {
         event: "Eat and Run",
         faculty: "Prof Soumik Mohapatra",
         facultyPhone: "7854837875",
-        studentCoordinators: ["Amrusha Priyadarsini", "Akshita Priyadarsini", "Subhaxmvi Duit", "Sneha Panda", "Bhawani Sahoo"],
-        contacts: ["8018092322", "8018200088", "9090177825", "8926333277", "9693809702"],
+        studentCoordinators: ["Amrusha Priyadarsini", "Akshita Priyadarsini", "Subhalaxmi Dixit ", "Sneha Panda", "Biswajit sahoo"],
+        contacts: ["8018092322", "8018200688", "9090177825", "8926333277", "9692890792"],
         year: "3rd"
     },
     {
@@ -140,16 +140,16 @@ const eventCoordinators = [
         event: "Marketing Madness",
         faculty: "Prof. Jyoti Panda",
         facultyPhone: "7735025428",
-        studentCoordinators: ["Abhijit Sahoo", "Abhijit Sahu", "Barisha Priyadarshi Panda", "Nirvaa Nayak"],
-        contacts: ["8249083223", "7848983424", "9777741077", "8260051182"],
+        studentCoordinators: ["Abhijit Sahoo", "Abhijit Sahu", "Barsha Priyadarshi Panda", "Priyanka Nayak"],
+        contacts: ["8249803223", "7846863424", "9777741077", "8260503182"],
         year: "2nd"
     },
     {
-        event: "Guest What!",
+        event: "Guess What!",
         faculty: "Prof. Smruti Das",
         facultyPhone: "8800853174",
-        studentCoordinators: ["Sudigraxnt Panda", "Barsha Barenya Nandi", "Sambartha Jayasingh", "Priya Shah", "Siddharth Senili"],
-        contacts: ["7735226841", "7439907553", "9610134238", "7936430184"],
+        studentCoordinators: ["Sudiptirani Panda", "Barsha Barenya Nandi", "Sambartika Jayasingh", "Priya Shah", "Sidhanta Swain"],
+        contacts: ["7735226841", "7439907553", "8926193812", "9681034238", "7205430911"],
         year: "2nd"
     }
 ];
@@ -198,7 +198,7 @@ export default function ContactPage() {
                     <h2 className="text-3xl font-bold text-orange-400 mb-6 font-serif">Event Details</h2>
                 </div>
 
-                {/* Events Table */}
+                Events Table
                 <div className="bg-gradient-to-br from-orange-600/20 to-black rounded-2xl shadow-2xl border border-orange-500 overflow-hidden">
                     <div className="overflow-x-auto">
                         <Table>
